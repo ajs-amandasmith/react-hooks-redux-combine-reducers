d5cfd63d88604aa22cd884f8f127e444ca50a2b5
Amanda Smith <ajs.amandasmith@gmail.com>
2022-11-18 20:42:41 -0600

    commit tree 6000c885cf4b6a9d5414d397cbf3c8c2534b4a74

---
+++ src/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import authorsReducer from "./features/authors/authorSlice";
+import authorsReducer from "./features/authors/authorSlice";
+import booksReducer from "./features/books/bookSlice";
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer,
+});
+
+export default rootReducer;
